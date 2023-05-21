2459eba091faa9f8e2bb3429143887d2cd6eafb1
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-21 10:50:11 +0100

    terraform

--- main.tf
+++ main.tf
@@ -37,3 +37,33 @@ resource "aws_lb" "test" {
     { Name = "${var.env}-alb"}
   )
 }
+
+resource "aws_lb" "load_balancer" {
+  name               = "${var.env}-${var.subnet_name}-alb"
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = var.subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.subnet_name}-alb" }
+  )
+}
+
+resource "aws_lb_listener" "backend" {
+  count = var.internal ? 1 : 0
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Not available"
+      status_code  = "502"
+    }
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "dns_name" {
   value = aws_lb.test.dns_name
 }
+
+output "listener" {
+  value = try(aws_lb_listener.backend.*.arn[0], null )
+}
